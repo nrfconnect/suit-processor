c3f2f3a77582482ef2e114966e37bfb029db1c54
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2022-09-12 16:53:49 +0200

    suit: Improve command sequence validation logic
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- src/suit_command_seq.c
+++ src/suit_command_seq.c
@@ -23,16 +23,217 @@ typedef union {
 
 int suit_validate_common_sequence(struct suit_processor_state *state, struct zcbor_string *cmd_seq_str)
 {
-	// SUS: Return statement required
-	return SUIT_SUCCESS;
+	size_t decoded_len;
+	suit_command_t result;
+	ZCBOR_STATE_D(d_state, 1, cmd_seq_str->value, cmd_seq_str->len, 1);
+
+	bool success = zcbor_list_start_decode(d_state);
+
+	if (!success) {
+		int ret = zcbor_peek_error(d_state);
+		return ZCBOR_ERR_TO_SUIT_ERR((ret != ZCBOR_SUCCESS) ? ret : ZCBOR_ERR_UNKNOWN);
+	}
+
+	if (d_state->indefinite_length_array) {
+		return SUIT_ERR_DECODING;
+	}
+
+	/* Each command brings a single argument, passed as an element inside the array,
+	   thus the number of commands is the half of the number of array elements */
+	if (d_state->elem_count % 2 == 1) {
+		return SUIT_ERR_DECODING;
+	}
+
+	for (int i = 0; i < d_state->elem_count / 2; i++) {
+		if (cbor_decode_SUIT_Condition(
+			d_state->payload, d_state->payload_end - d_state->payload,
+			&result.condition, &decoded_len) == ZCBOR_SUCCESS) {
+			int retval;
+
+			if (i == 0) {
+				/* If there is only one component, it is valid to skip the set-component-index command */
+				if (state->num_components != 1) {
+					/* Each sequence should begin with a set-component-index command */
+					return SUIT_ERR_MANIFEST_VALIDATION;
+				}
+			}
+
+			for (int j = 0; j < state->num_components; j++) {
+				if (state->current_components[j]) {
+					switch (result.condition._SUIT_Condition_choice) {
+					case _SUIT_Condition___suit_condition_vendor_identifier:
+					case _SUIT_Condition___suit_condition_class_identifier:
+					case _SUIT_Condition___suit_condition_device_identifier:
+					case _SUIT_Condition___suit_condition_image_match:
+					case _SUIT_Condition___suit_condition_component_slot:
+					case _SUIT_Condition___suit_condition_abort:
+						retval = SUIT_SUCCESS;
+						break;
+					default:
+						retval = SUIT_ERR_MANIFEST_VALIDATION;
+						break;
+					}
+					if (retval != SUIT_SUCCESS) {
+						return retval;
+					}
+				}
+			}
+			d_state->payload += decoded_len;
+			continue;
+		}
+
+		if (cbor_decode_SUIT_Directive(
+			d_state->payload, d_state->payload_end - d_state->payload,
+			&result.directive, &decoded_len) == ZCBOR_SUCCESS) {
+			int retval;
+
+			if (i == 0) {
+				/* If there is only one component, it is valid to skip the set-component-index command */
+				if ((state->num_components != 1) &&
+				    (result.directive._SUIT_Directive_choice
+					!= _SUIT_Directive___suit_directive_set_component_index)) {
+					/* Each sequence should begin with a set-component-index command */
+					return SUIT_ERR_MANIFEST_VALIDATION;
+				}
+			}
+
+			switch (result.directive._SUIT_Directive_choice) {
+			case _SUIT_Directive___suit_directive_set_component_index:
+			case _SUIT_Directive___suit_directive_try_each:
+			case _SUIT_Directive___suit_directive_override_parameters:
+				retval = SUIT_SUCCESS;
+				break;
+			case _SUIT_Directive___suit_directive_fetch:
+			case _SUIT_Directive___suit_directive_copy:
+			case _SUIT_Directive___suit_directive_run:
+			default:
+				retval = SUIT_ERR_MANIFEST_VALIDATION;
+				break;
+			}
+			if (retval != SUIT_SUCCESS) {
+				return retval;
+			}
+			d_state->payload += decoded_len;
+			continue;
+		}
+
+		/* Should not come here. */
+		return SUIT_ERR_DECODING;
+	}
+
+	if (d_state->payload == d_state->payload_end) {
+		return SUIT_SUCCESS;
+	}
+
+	return SUIT_ERR_DECODING;
 }
 
 
 int suit_validate_command_sequence(struct suit_processor_state *state, struct zcbor_string *cmd_seq_str)
 {
-	state->dry_run = suit_bool_true;
-	// SUS: Return statement required
-	return SUIT_SUCCESS;
+	size_t decoded_len;
+	suit_command_t result;
+	ZCBOR_STATE_D(d_state, 1, cmd_seq_str->value, cmd_seq_str->len, 1);
+
+	bool success = zcbor_list_start_decode(d_state);
+
+	if (!success) {
+		int ret = zcbor_peek_error(d_state);
+		return ZCBOR_ERR_TO_SUIT_ERR((ret != ZCBOR_SUCCESS) ? ret : ZCBOR_ERR_UNKNOWN);
+	}
+
+	if (d_state->indefinite_length_array) {
+		return SUIT_ERR_DECODING;
+	}
+
+	/* Each command brings a single argument, passed as an element inside the array,
+	   thus the number of commands is the half of the number of array elements */
+	if (d_state->elem_count % 2 == 1) {
+		return SUIT_ERR_DECODING;
+	}
+
+	for (int i = 0; i < d_state->elem_count / 2; i++) {
+		if (cbor_decode_SUIT_Condition(
+			d_state->payload, d_state->payload_end - d_state->payload,
+			&result.condition, &decoded_len) == ZCBOR_SUCCESS) {
+			int retval;
+
+			if (i == 0) {
+				/* If there is only one component, it is valid to skip the set-component-index command */
+				if (state->num_components != 1) {
+					/* Each sequence should begin with a set-component-index command */
+					return SUIT_ERR_MANIFEST_VALIDATION;
+				}
+			}
+
+			for (int j = 0; j < state->num_components; j++) {
+				if (state->current_components[j]) {
+					switch (result.condition._SUIT_Condition_choice) {
+					case _SUIT_Condition___suit_condition_vendor_identifier:
+					case _SUIT_Condition___suit_condition_class_identifier:
+					case _SUIT_Condition___suit_condition_device_identifier:
+					case _SUIT_Condition___suit_condition_image_match:
+					case _SUIT_Condition___suit_condition_component_slot:
+					case _SUIT_Condition___suit_condition_abort:
+						retval = SUIT_SUCCESS;
+						break;
+					default:
+						retval = SUIT_ERR_MANIFEST_VALIDATION;
+						break;
+					}
+					if (retval != SUIT_SUCCESS) {
+						return retval;
+					}
+				}
+			}
+			d_state->payload += decoded_len;
+			continue;
+		}
+
+		if (cbor_decode_SUIT_Directive(
+			d_state->payload, d_state->payload_end - d_state->payload,
+			&result.directive, &decoded_len) == ZCBOR_SUCCESS) {
+			int retval;
+
+			if (i == 0) {
+				/* If there is only one component, it is valid to skip the set-component-index command */
+				if ((state->num_components != 1) &&
+				    (result.directive._SUIT_Directive_choice
+					!= _SUIT_Directive___suit_directive_set_component_index)) {
+					/* Each sequence should begin with a set-component-index command */
+					return SUIT_ERR_MANIFEST_VALIDATION;
+				}
+			}
+
+			switch (result.directive._SUIT_Directive_choice) {
+			case _SUIT_Directive___suit_directive_set_component_index:
+			case _SUIT_Directive___suit_directive_try_each:
+			case _SUIT_Directive___suit_directive_override_parameters:
+			case _SUIT_Directive___suit_directive_fetch:
+			case _SUIT_Directive___suit_directive_copy:
+			case _SUIT_Directive___suit_directive_run:
+				retval = SUIT_SUCCESS;
+				break;
+			default:
+				retval = SUIT_ERR_MANIFEST_VALIDATION;
+				break;
+			}
+			if (retval != SUIT_SUCCESS) {
+				return retval;
+			}
+			d_state->payload += decoded_len;
+			continue;
+		}
+
+		/* Should not come here. */
+		return SUIT_ERR_DECODING;
+	}
+
+	if (d_state->payload == d_state->payload_end) {
+		return SUIT_SUCCESS;
+	}
+
+	return SUIT_ERR_DECODING;
 }
 
 
@@ -47,15 +248,6 @@ int suit_run_command_sequence(struct suit_processor_state *state, struct zcbor_s
 	if (!success) {
 		int ret = zcbor_peek_error(d_state);
 		return ZCBOR_ERR_TO_SUIT_ERR((ret != ZCBOR_SUCCESS) ? ret : ZCBOR_ERR_UNKNOWN);
-	}
-	if (d_state->indefinite_length_array) {
-		return SUIT_ERR_DECODING;
-	}
-
-	// SUS: Each command brings a single argument, passed as an elemnt inside the array,
-	//      thus the number of commands is the half of the number of array elements.
-	if (d_state->elem_count % 2 == 1) {
-		return SUIT_ERR_DECODING;
 	}
 
 	for (int i = 0; i < d_state->elem_count / 2; i++) {
@@ -165,4 +357,30 @@ int suit_run_command_sequence(struct suit_processor_state *state, struct zcbor_s
 	}
 
 	return SUIT_ERR_DECODING;
+}
+
+
+int suit_run_common_sequence(struct suit_processor_state *state, struct zcbor_string *cmd_seq_str)
+{
+	int err = SUIT_SUCCESS;
+
+	/* Reset SUIT parameters */
+	for (int i = 0; i < state->num_components; i++) {
+		suit_reset_params(&state->components[i]);
+	}
+
+	/* If there is only one component - set it as an active one */
+	if (state->num_components == 1) {
+		struct IndexArg_ index_arg = {
+			._IndexArg_uint = 0,
+			._IndexArg_choice = _IndexArg_uint,
+		};
+		err = suit_directive_set_current_components(state, &index_arg);
+	}
+
+	if (err == SUIT_SUCCESS) {
+		err = suit_run_command_sequence(state, cmd_seq_str);
+	}
+
+	return err;
 }
