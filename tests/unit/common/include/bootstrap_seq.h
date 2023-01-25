d72bf14f3f823c51d645755152da994d7ad79537
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2023-01-25 17:17:44 +0100

    tests: Add utilities for testing command sequences
    The utilities allow to configure SUIT processor state in such a way,
    that running single command sequences is possible, without executing the
    full envelope validation sequence.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

---
+++ tests/unit/common/include/bootstrap_seq.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef BOOTSTRAP_SEQ_H__
+#define BOOTSTRAP_SEQ_H__
+
+#include <stdint.h>
+#include <string.h>
+
+
+/** @brief Sequence header bootstraping function prototype.
+ *
+ * @param  cmd_buf       Buffer to modify.
+ * @param  buf_size      Size of the destination buffer.
+ * @param  content_size  Size of the contents, already populated inside the buffer.
+ *
+ * @returns The new content size.
+ */
+typedef size_t (*bootsrap_seq_hdr_generator_t)(uint8_t * cmd_buf, size_t buf_size, size_t content_size);
+
+/** @brief Bootstrap command sequence header with directive-run-sequence.
+ *
+ * @note The payload passed as argument is attached as the directive-run-sequence body.
+ *
+ * @param  cmd_buf       Buffer to modify.
+ * @param  buf_size      Size of the destination buffer.
+ * @param  content_size  Size of the contents, already populated inside the buffer.
+ *
+ * @returns The new content size.
+ */
+size_t bootsrap_seq_hdr_run_sequence(uint8_t * cmd_buf, size_t buf_size, size_t content_size);
+
+/** @brief Bootstrap command sequence header with directive-run-sequence and set-component-index set to true.
+ *
+ * @note The payload passed as argument is attached as the directive-run-sequence body.
+ *
+ * @param  cmd_buf       Buffer to modify.
+ * @param  buf_size      Size of the destination buffer.
+ * @param  content_size  Size of the contents, already populated inside the buffer.
+ *
+ * @returns The new content size.
+ */
+size_t bootsrap_seq_hdr_run_sequence_on_all_components(uint8_t * cmd_buf, size_t buf_size, size_t content_size);
+
+/** @brief Bootstrap command sequence header with directive-try-each.
+ *
+ * @note The payload passed as argument is attached as the directive-run-sequence body.
+ *
+ * @param  cmd_buf       Buffer to modify.
+ * @param  buf_size      Size of the destination buffer.
+ * @param  content_size  Size of the contents, already populated inside the buffer.
+ *
+ * @returns The new content size.
+ */
+size_t bootsrap_seq_hdr_try_each(uint8_t * cmd_buf, size_t buf_size, size_t content_size);
+
+/** @brief Bootstrap command sequence header with directive-try-each and set-component-index set to true.
+ *
+ * @note The payload passed as argument is attached as the directive-run-sequence body.
+ *
+ * @param  cmd_buf       Buffer to modify.
+ * @param  buf_size      Size of the destination buffer.
+ * @param  content_size  Size of the contents, already populated inside the buffer.
+ *
+ * @returns The new content size.
+ */
+size_t bootsrap_seq_hdr_try_each_on_all_components(uint8_t * cmd_buf, size_t buf_size, size_t content_size);
+
+#endif /* BOOTSTRAP_SEQ_H__ */
