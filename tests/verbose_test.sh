78874fe1f392795901267c66f4082f398923224a
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2023-05-25 10:29:25 +0200

    core: Change license to LicenseRef-Nordic-5-Clause
    This commit changes the license from Apache-2.0 to Nordic-5-Clause for
    all files inside the suit-processor repository.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- tests/verbose_test.sh
+++ tests/verbose_test.sh
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2022 Nordic Semiconductor ASA
 #
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
 ./test.sh -x VERBOSE=ON -x ASSERTS=ON $*
